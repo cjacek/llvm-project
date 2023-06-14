fde97bd6ed14e7200c5e81339d8eaaf2e07745a4
Jacek Caban <jacek@codeweavers.com>
2023-06-14 22:03:25 +0200

    [lld] Always mark ARM64EC modules as instrumented.
    FIXME: The test doesn't seem to work on MSVC, but it's observed in real-world images, needs more investigation.

--- lld/COFF/Writer.cpp
+++ lld/COFF/Writer.cpp
@@ -1152,8 +1152,7 @@ void Writer::createMiscChunks() {
     createSEHTable();
 
   // Create /guard:cf tables if requested.
-  if (config->guardCF != GuardCFLevel::Off)
-    createGuardCFTables();
+  createGuardCFTables();
 
   if (isArm64EC(config->machine))
     createECChunks();
@@ -1901,6 +1900,15 @@ void Writer::markSymbolsWithRelocations(ObjFile *file,
 // table.
 void Writer::createGuardCFTables() {
   Configuration *config = &ctx.config;
+
+  if (config->guardCF == GuardCFLevel::Off) {
+    if (isArm64EC(config->machine)) {
+      Symbol *flagSym = ctx.symtab.findUnderscore("__guard_flags");
+      cast<DefinedAbsolute>(flagSym)->setVA(
+          uint32_t(GuardFlags::CF_INSTRUMENTED));
+    }
+    return;
+  }
 
   SymbolRVASet addressTakenSyms;
   SymbolRVASet giatsRVASet;
