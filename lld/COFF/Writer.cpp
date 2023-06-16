f8a195349701fb7b3cc873ff8d054f3c65a607dd
Jacek Caban <jacek@codeweavers.com>
2023-06-16 20:05:39 +0200

    [lld] Generate X64 thunks for ARM64EC entry points and patchable functions.

--- lld/COFF/Writer.cpp
+++ lld/COFF/Writer.cpp
@@ -304,6 +304,7 @@ private:
   uint64_t sizeOfHeaders;
 
   OutputSection *textSec;
+  OutputSection *hexpthkSec;
   OutputSection *rdataSec;
   OutputSection *buildidSec;
   OutputSection *dataSec;
@@ -984,6 +985,7 @@ void Writer::createSections() {
 
   // Try to match the section order used by link.exe.
   textSec = createSection(".text", code | r | x);
+  hexpthkSec = createSection(".hexpthk", code | r | x);
   createSection(".bss", bss | r | w);
   rdataSec = createSection(".rdata", data | r);
   buildidSec = createSection(".buildid", data | r);
@@ -2050,6 +2052,15 @@ void Writer::maybeAddRVATable(SymbolRVASet tableSymbols, StringRef tableSym,
 
 // Create CHPE metadata chunks.
 void Writer::createECChunks() {
+  for (auto iter = ctx.ECThunks.begin(); iter != ctx.ECThunks.end();) {
+    if ((*iter)->target->getChunk()->live) {
+      hexpthkSec->addChunk(*iter);
+      iter++;
+    } else {
+      iter = ctx.ECThunks.erase(iter);
+    }
+  }
+
   auto codeMapChunk = make<ECCodeMapChunk>(codeMap);
   rdataSec->addChunk(codeMapChunk);
   Symbol *codeMapSym = ctx.symtab.findUnderscore("__hybrid_code_map");
