66e0f48350d7f3e568a85252d8f0c4fda61cdf85
Jacek Caban <jacek@codeweavers.com>
2023-06-23 17:43:27 +0200

    [WIP] [lld] Add support for EC entry thunks.

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -2656,6 +2656,9 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
     doICF(ctx);
   }
 
+  if (ctx.config.machine == ARM64EC)
+    ctx.symtab.initializeEntryThunks();
+
   // Write the result.
   writeResult(ctx);
 
