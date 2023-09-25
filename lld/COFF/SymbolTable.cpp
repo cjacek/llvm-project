0b2f807d13339bab1cc7ae09c08ce63e480946b7
Jacek Caban <jacek@codeweavers.com>
2023-09-25 18:44:29 +0200

    [lld][COFF] Add some EC alias tests and fixes.

--- lld/COFF/SymbolTable.cpp
+++ lld/COFF/SymbolTable.cpp
@@ -603,7 +603,9 @@ void SymbolTable::initializeEntryThunks() {
 Symbol *SymbolTable::addUndefined(StringRef name, InputFile *f,
                                   bool isWeakAlias) {
   auto [s, wasInserted] = insert(name, f);
-  if (wasInserted || (s->isLazy() && isWeakAlias)) {
+  if (wasInserted ||
+      (s->isLazy() && isWeakAlias &&
+       (!isa<Undefined>(s) || !cast<Undefined>(s)->isECAlias()))) {
     replaceSymbol<Undefined>(s, name);
     return s;
   }
@@ -620,10 +622,12 @@ void SymbolTable::addLazyArchive(ArchiveFile *f, const Archive::Symbol &sym) {
     return;
   }
   auto *u = dyn_cast<Undefined>(s);
-  if (!u || u->weakAlias || s->pendingArchiveLoad)
+  if (!u || (u->weakAlias && !u->isECAlias()) || s->pendingArchiveLoad)
     return;
   s->pendingArchiveLoad = true;
   f->addMember(sym);
+  if (u->ECAlias)
+    u->ECAlias->pendingArchiveLoad = true;
 }
 
 void SymbolTable::addLazyObject(InputFile *f, StringRef n) {
