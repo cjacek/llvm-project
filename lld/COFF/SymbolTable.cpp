03ceb8be6082cedb27aa2ac1d5a4cbc3b427d653
Jacek Caban <jacek@codeweavers.com>
2023-10-17 16:15:18 +0200

    [lld] Add support for EC symbols in local import thunks.

--- lld/COFF/SymbolTable.cpp
+++ lld/COFF/SymbolTable.cpp
@@ -18,6 +18,7 @@
 #include "lld/Common/Timer.h"
 #include "llvm/DebugInfo/DIContext.h"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Mangler.h"
 #include "llvm/LTO/LTO.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/Parallel.h"
@@ -502,7 +503,15 @@ bool SymbolTable::resolveRemainingUndefines() {
     // If we can resolve a symbol by removing __imp_ prefix, do that.
     // This odd rule is for compatibility with MSVC linker.
     if (name.starts_with("__imp_")) {
-      Symbol *imp = find(name.substr(strlen("__imp_")));
+      StringRef impName = name.substr(strlen("__imp_"));
+      if (isArm64EC(ctx.config.machine))
+        impName.consume_front("aux_");
+      Symbol *imp = find(impName);
+      if (isArm64EC(ctx.config.machine) && (!imp || !isa<Defined>(imp))) {
+        if (std::optional<std::string> MangledName =
+                getArm64ECMangledFunctionName(impName))
+          imp = find(*MangledName);
+      }
       if (imp) {
         // The unprefixed symbol might come later in symMap, so handle it now
         // so that the condition below can be appropriately applied.
