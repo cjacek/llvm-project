e69ab590d65dd08cbb5f7c47622151f159cd3d56
Jacek Caban <jacek@codeweavers.com>
2023-10-31 23:57:33 +0100

    [lld] Add support for __impchk_* symbols.

--- lld/COFF/Writer.cpp
+++ lld/COFF/Writer.cpp
@@ -1240,8 +1240,8 @@ void Writer::appendImportThunks() {
         textSec->addChunk(chunk);
     }
 
-    if (file->ECThunk)
-      textSec->addChunk(file->ECThunk);
+    if (file->chkECSym)
+      textSec->addChunk(file->chkECSym->getChunk());
   }
 
   if (!delayIdata.empty()) {
