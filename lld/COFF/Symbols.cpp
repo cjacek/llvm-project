b705a93066797b0f7943df1657ec0ee5154e31f2
Billy Laws <blaws05@gmail.com>
2024-07-27 15:40:09 +0000

    [LLD] Disallow chaining of antidep aliases
    link.exe also disallows this, as an unmangled symbol could resolve to
    its own exit thunk and generate an infinite loop.

--- lld/COFF/Symbols.cpp
+++ lld/COFF/Symbols.cpp
@@ -135,6 +135,8 @@ Defined *Undefined::getWeakAlias() {
   // A weak alias may be a weak alias to another symbol, so check recursively.
   SmallSet<Symbol *, 4> weakChain;
   for (Symbol *a = weakAlias; a; a = cast<Undefined>(a)->weakAlias) {
+    if (a->isAntiDep)
+      break;
     if (weakChain.contains(a))
       break; // We have a cycle.
     if (auto *d = dyn_cast<Defined>(a))
@@ -156,6 +158,7 @@ bool Undefined::resolveWeakAlias() {
   // Symbols. For that reason we need to check which type of symbol we
   // are dealing with and copy the correct number of bytes.
   StringRef name = getName();
+  bool wasAntiDep = isAntiDep;
   if (isa<DefinedRegular>(d))
     memcpy(this, d, sizeof(DefinedRegular));
   else if (isa<DefinedAbsolute>(d))
@@ -165,6 +168,7 @@ bool Undefined::resolveWeakAlias() {
 
   nameData = name.data();
   nameSize = name.size();
+  isAntiDep = isAntiDep || wasAntiDep;
   return true;
 }
 
